bac6c813d664ed8a5300b8bd15e762f0d02c196c
carranz998 <carranz998@gmail.com>
2020-12-24 02:27:25 +0100

    Clean checklist, dirty code

--- DinnerPaymentAlgorithm.py
+++ DinnerPaymentAlgorithm.py
@@ -6,10 +6,12 @@ class DinnerPaymentAlgorithm:
     def __init__(self):
         self.persons = [p for p in PersonBuilder().generate_person()]
 
+        for p in self.persons:
+            print(p)
 
     def calcule_transactions(self):
 
-        self.print_change_taking()
+        print_change_taking()
 
         creditors, debtors = [], []
 
@@ -27,24 +29,16 @@ class DinnerPaymentAlgorithm:
 
                 if debtor_give_all(d, c) and d.debt * c.debt < 0:
                     print_money_giving(d.name, abs(c.debt), c.name)
-                    d.debt, c.debt = self.transaction(d.debt, c.debt)
+                    d.debt, c.debt = transaction(d.debt, c.debt)
 
                 elif creditor_gets_all(d, c) and d.debt * c.debt < 0:
                     print_money_giving(d.name, d.debt, c.name)
-                    c.debt, d.debt = self.transaction(d.debt, c.debt)
-
-    
-    def debtor_give_all(self, d, c):
-        return d.debt > abs(c.debt)
+                    c.debt, d.debt = transaction(d.debt, c.debt)
 
 
-    def transaction(self, q1, q2):
-        return round(q1 + q2, 2), 0 
-
-
-    def print_change_taking(self):
-        for p in self.persons:
-            print(p.name + " takes " + str(p.change) + " from the change.")
+def print_change_taking():
+    for p in self.persons:
+        print(p.name + " takes " + str(p.change) + " from the change.")
 
 
 def debtor_give_all(d, c):
@@ -57,4 +51,8 @@ def creditor_gets_all(d, c):
 
 def print_money_giving(issuer, quantity, receiver):
     print(issuer + ' gives ' + str(quantity) + ' to ' + receiver)
+
+
+def transaction(q1, q2):
+    return round(q1 + q2, 2), 0
 
